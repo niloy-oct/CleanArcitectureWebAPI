b2c58a21184c18746e0d2de363ea651dc2146643
Mahedi Hasan Niloy <139734574+hpl-sd-niloy@users.noreply.github.com>
2023-11-11 21:19:40 +0600

    setup initial program.cs file and setup connection with MSSQL

--- SalesPlusAPI/Program.cs
+++ SalesPlusAPI/Program.cs
@@ -1,5 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("LocalSqlServer");
+
+builder.Services.AddDbContext<APIDbContext>(opt => opt.UseSqlServer(connection));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 
 var app = builder.Build();
