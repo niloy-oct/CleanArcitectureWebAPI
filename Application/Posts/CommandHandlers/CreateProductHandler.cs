8b7bb855965f44d45e0fa5424173347cefccc949
Mahedi Hasan Niloy <139734574+hpl-sd-niloy@users.noreply.github.com>
2023-11-11 21:36:22 +0600

    setup MediaTR

---
+++ Application/Posts/CommandHandlers/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Abstractions;
+using Application.Posts.Commands;
+using Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Posts.CommandHandlers
+{
+    public class CreateProductHandler : IRequestHandler<CreateProduct, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<Product> Handle(CreateProduct request, CancellationToken cancellationToken)
+        {
+            var newproduct = new Product
+            {
+                ProductCode = request.ProductCode,
+                ProductName = request.ProductName
+            };
+
+            return await _productRepository.CreateProduct(newproduct);
+        }
+   
+    }
+}
---
+++ Application/Posts/Commands/CreateProduct.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Posts.Commands
+{
+    public class CreateProduct : IRequest<Product>
+    {
+        public string? ProductCode { get; set; }
+        public string? ProductName { get; set; }
+    }
+}
